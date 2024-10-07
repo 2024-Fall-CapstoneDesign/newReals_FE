6806a461a13c38e499e57282f16829f23f406980
yeona <aya813@naver.com>
2024-10-07 22:42:16 +0900

    #1 [feat] : router 설정

---
+++ src/AppRouter.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Intro from './pages/Intro';
+import Home from './pages/Home';
+import Category from './pages/Category';
+import Interest from './pages/Interest';
+import Market from './pages/Market';
+import NewsDetail from './pages/NewsDetail';
+import Profile from './pages/Profile';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/interest" element={<Interest />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/newDetail/:id" element={<NewsDetail />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/pages/Intro.tsx
@@ -0,0 +1,5 @@
+const Intro = () => {
+  return <div>인트로 페이지입니다.</div>;
+};
+
+export default Intro;
