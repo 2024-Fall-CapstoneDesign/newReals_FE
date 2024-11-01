71871b85ed5bc316ebded8a2d0dcbc09a1585f2b
yeona <aya813@naver.com>
2024-11-02 02:11:50 +0900

    #14 [refactor] : 1280 사이즈에 맞춰 다시 크기 조정

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -3,7 +3,7 @@ import * as S from './ButtonStyle';
 interface ButtonProps {
   children: React.ReactNode;
   onClick: () => void;
-  type?: 'quiz';
+  type?: 'quiz' | 'modal';
 }
 
 const Button = ({ children, onClick, type }: ButtonProps) => {
--- src/components/newsDetail/quiz/QuizStyle.tsx
+++ src/components/newsDetail/quiz/QuizStyle.tsx
@@ -4,16 +4,16 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  max-width: 33.3125rem;
-  padding: 1.5rem;
+  max-width: 21.6875rem;
+  padding: 1.25rem;
   background-color: var(--grayscale-5);
-  border-radius: 1rem;
+  border-radius: 0.75rem;
   color: var(--black);
   gap: 0.75rem;
 `;
 
 export const Quiz = styled.span`
-  font-size: 1.5rem;
+  font-size: 1rem;
   font-weight: 700;
   line-height: 150%;
 `;
@@ -26,15 +26,15 @@ export const Q = styled.span`
 export const Content = styled.section`
   display: flex;
   flex-direction: column;
-  padding: 1.25rem;
+  padding: 1rem;
   background-color: var(--main-0);
-  border-radius: 0.75rem;
-  gap: 1rem;
+  border-radius: 0.5rem;
+  gap: 0.75rem;
 `;
 
 export const Text = styled.p`
   opacity: 0.68;
-  font-size: 1.125rem;
+  font-size: 0.875rem;
   font-weight: 600;
   line-height: 150%;
 `;
