efd4f998f3c097dd6a336360c0afcc94af1f807c
Jina <jinatye@naver.com>
2024-11-04 22:27:01 +0900

    #19 [refactor] 수정사항 반영완료

--- src/components/newsDetail/aiSummary/AISummary.tsx
+++ src/components/newsDetail/aiSummary/AISummary.tsx
@@ -3,8 +3,12 @@ import DownArrowIcon from '../../../assets/icons/DownArrowIcon.svg';
 import UpArrowIcon from '../../../assets/icons/UpArrowIcon.svg';
 import { useState } from 'react';
 
-const AISummary = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+interface AISummary {
+  text: string;
+}
+
+const AISummary = ({ text }: AISummary) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
     setIsOpen((prev) => !prev);
@@ -14,18 +18,13 @@ const AISummary = () => {
     <S.AIPart>
       <S.FixedPart>
         <S.PartName>AI 요약 보기</S.PartName>
-        {isOpen ? (
-          <S.ToggleImg onClick={handleToggle} src={UpArrowIcon} alt="접어두기" />
-        ) : (
-          <S.ToggleImg onClick={handleToggle} src={DownArrowIcon} alt="펼쳐보기" />
-        )}
+        <S.ToggleImg
+          onClick={handleToggle}
+          src={isOpen ? UpArrowIcon : DownArrowIcon}
+          alt="토글 버튼"
+        />
       </S.FixedPart>
-      {isOpen && (
-        <S.Content>
-          윤석열 대통령이 필리핀을 국빈 방문해 무역·투자·에너지 등 협력 강화 및 동포 사회 이익을
-          논의하고, 아세안 정상회의에도 참석할 예정입니다.
-        </S.Content>
-      )}
+      {isOpen && <S.Content>{text}아아에이엥</S.Content>}
     </S.AIPart>
   );
 };
