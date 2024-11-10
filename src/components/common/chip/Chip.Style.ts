a43766600cf49fd0a3cc54f9a56af69a18e32de7
yeona <aya813@naver.com>
2024-11-11 01:15:08 +0900

    #38 [feat] : Chip 마이페이지에서도 사용할 수 있도록 수정

--- src/components/common/chip/Chip.Style.ts
+++ src/components/common/chip/Chip.Style.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 import { Colors, FontStyles } from '../../../styles';
 
-export const Chip = styled.div<{ $isSelected: boolean }>`
-  ${FontStyles.SM_Regular}
+export const Chip = styled.div<{ $isSelected: boolean; $size?: 'small' | 'large' }>`
+  ${({ $size }) => ($size === 'small' ? FontStyles.XS_Medium : FontStyles.SM_SemiBold)}
   width: fit-content;
-  padding: 0.5rem 1rem;
+  padding: ${({ $size }) => ($size === 'small' ? '0.25rem 0.75rem' : '0.5rem 1rem')};
   border-radius: 25rem;
   border: 1px solid ${({ $isSelected }) => ($isSelected ? Colors.Main50 : 'none')};
   background-color: ${({ $isSelected }) => ($isSelected ? Colors.Main5 : Colors.Grayscale5)};
