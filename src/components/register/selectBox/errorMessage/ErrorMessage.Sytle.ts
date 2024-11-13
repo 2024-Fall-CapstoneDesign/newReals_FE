308cb9b147a02238b69cd8ee532253c1d49d9b99
yeona <aya813@naver.com>
2024-11-13 11:05:00 +0900

    #49 [refactor] : 폴더 구조 정리

--- src/components/register/selectBox/errorMessage/ErrorMessage.Sytle.ts
+++ src/components/register/selectBox/errorMessage/ErrorMessage.Sytle.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Colors, FontStyles } from '../../../styles';
+import { Colors, FontStyles } from '../../../../styles';
 
 export const ErrorMessage = styled.div`
   display: flex;
