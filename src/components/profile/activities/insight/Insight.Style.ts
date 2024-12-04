115cf73247aec1b3c6aa7ba46ccd3dbed8c55959
yeona <aya813@naver.com>
2024-12-04 23:39:38 +0900

    #124 [feat] : 인사이트 리스트 없을 경우 구현

--- src/components/profile/activities/insight/Insight.Style.ts
+++ src/components/profile/activities/insight/Insight.Style.ts
@@ -29,3 +29,32 @@ export const Insight = styled.div`
     display: none;
   }
 `;
+
+export const NoContent = styled.div`
+  ${FontStyles.MD2_Medium}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3rem 1.25rem;
+  gap: 0.75rem;
+  background-color: ${Colors.Grayscale5};
+  border-radius: 1rem;
+  width: 100%;
+  color: ${Colors.Grayscale60};
+`;
+
+export const Icon = styled.img`
+  width: 2.25rem;
+  height: 2.25rem;
+`;
+
+export const Button = styled.button`
+  ${FontStyles.SM_SemiBold}
+  width: fit-content;
+  padding: 0.5rem 0.75rem;
+  border-radius: 0.5rem;
+  background-color: ${Colors.Main0};
+  color: ${Colors.Grayscale50};
+  border: none;
+  cursor: pointer;
+`;
