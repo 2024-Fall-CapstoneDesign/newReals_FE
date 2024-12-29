d1068016bdeb26577c023c469280918f5541454d
Jina <jinatye@naver.com>
2024-12-29 16:07:54 +0900

    #146 [feat] : 헤더 반응형 UI 구현

--- src/components/common/header/Header.Style.ts
+++ src/components/common/header/Header.Style.ts
@@ -2,6 +2,17 @@ import styled from 'styled-components';
 import MarketIcon from '../../../assets/icons/MarketIcon.svg?react';
 import { Colors, FontStyles } from '../../../styles';
 
+export const MobileHeaderContainer = styled.div`
+  position: relative;
+  width: 100%;
+  background-color: #fff; /* 배경색 */
+  z-index: 10; /* 모바일 메뉴보다 위로 배치 */
+`;
+
+export const SearchBarContainer = styled.div`
+  padding: 0rem 1rem 1rem 1rem;
+`;
+
 export const Head = styled.header`
   padding: 0.75rem 6.25rem 0.75rem 6.25rem;
   display: flex;
@@ -10,11 +21,17 @@ export const Head = styled.header`
   width: 100%;
   justify-content: space-between;
   border-bottom: 1px solid ${Colors.Grayscale10};
+  @media (max-width: 767px) {
+    padding: 0.75rem 1rem 0.75rem 1rem;
+    height: 4rem;
+    position: relative;
+    border-bottom: none;
 `;
 
 export const Service = styled.span`
   display: flex;
   align-items: center;
+  cursor: pointer;
 `;
 
 export const ServiceName = styled.span`
@@ -29,7 +46,35 @@ export const ServiceName = styled.span`
 export const ServiceImg = styled.img`
   width: 2.5625rem;
   height: 1.75rem;
+`;
+
+export const SearchImg = styled.img``;
+
+export const MobileEtc = styled.img``;
+
+export const MobileMenu = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
+  border-top: 1px solid ${Colors.Grayscale10};
+`;
+
+export const MobileMenuItem = styled.div`
+  padding: 0.75rem 1rem;
+  font-size: 1rem;
   cursor: pointer;
+  color: ${Colors.Grayscale90};
+  ${FontStyles.MD2_Medium};
+  line-height: 140%;
+  &:hover {
+    background-color: #f7f7f7;
+  }
 `;
 
 export const Category = styled.div`
@@ -68,6 +113,9 @@ export const SidePart = styled.div`
   gap: 3rem;
   align-items: center;
   justify-content: center;
+  @media (max-width: 767px) {
+    gap: 1rem;
+  }
 `;
 
 export const Etc = styled.div`
