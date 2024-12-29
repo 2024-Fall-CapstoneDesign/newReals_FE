09ac461042e2cf06f5062441503bf71f95ee98c0
Jina <jinatye@naver.com>
2024-12-29 18:18:23 +0900

    #152 [feat] : 인트로 페이지 반응형 UI 구현

--- src/components/intro/layout/CommonLayout.Style.ts
+++ src/components/intro/layout/CommonLayout.Style.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { Colors } from '../../../styles';
 
+export const ImgPart = styled.img`
+  margin-top: auto;
+`;
+
 export const IntroMent = styled.div<{ isLeft: boolean }>`
   display: flex;
   flex-direction: ${({ isLeft }) => (isLeft ? 'row' : 'row-reverse')};
@@ -11,8 +15,17 @@ export const IntroMent = styled.div<{ isLeft: boolean }>`
   align-self: stretch;
   background-color: ${({ isLeft }) => (isLeft ? Colors.Grayscale5 : Colors.Main0)};
   gap: 6rem;
-`;
 
-export const ImgPart = styled.img`
-  margin-top: auto;
+  /* 모바일에서 이미지 숨기기 */
+  @media (max-width: 767px) {
+    height: 14rem;
+    /* 모바일 화면에서 이미지 숨기기 */
+    ${ImgPart} {
+      display: none;
+    }
+
+    /* 모바일에서 텍스트가 더 커지도록 조정 */
+    padding: 0 2rem;
+    gap: 2rem;
+  }
 `;
