79cddddee154445cb3afa80616b0bad80f8608e1
yeona <aya813@naver.com>
2024-12-29 16:34:24 +0900

    #143 [feat] : 관심사 선택 페이지 반응형 UI 구현 완료

--- src/components/common/header/Header.Style.ts
+++ src/components/common/header/Header.Style.ts
@@ -26,21 +26,7 @@ export const Head = styled.header`
     height: 4rem;
     position: relative;
     border-bottom: none;
-`;
-
-export const Service = styled.span`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-`;
-
-export const ServiceName = styled.span`
-  color: ${Colors.Grayscale90};
-  font-family: 'Couture';
-  font-size: 1.15rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
+  }
 `;
 
 export const Service = styled.span`
